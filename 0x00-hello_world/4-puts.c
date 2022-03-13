392b297323039b1fd93f9f3bf6c0108b10b730d4
Iamtunny <maestrotunexxxie@gmail.com>
2022-03-13 08:51:35 -0700

    new commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,8 +4,11 @@
  *
  *Return: 0 after printing the function
  */
+
 int main(void)
+
 {
-	puts("\"programming is like building a multilingual puzzle");
-	return (0);
+puts("programming is like building a multilingual puzzle");
+
+return (0);
 }
