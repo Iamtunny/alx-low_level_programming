8401de75f595346684c7dd89a8a4635bd474df8b
Iamtunny <maestrotunexxxie@gmail.com>
2022-03-17 09:36:29 -0700

    new commits

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
-
- /**
+/**
  * main - causes an infinite loop
+ *
  * Return: 0
  */
-
 int main(void)
 {
-
 int i;
 
-printf("infinite loop incoming :(\n");
+printf("Infinite loop incoming :(\n");
 
 i = 0;
 
@@ -22,6 +20,5 @@ i = 0;
  }
  */
 printf("Infinite loop avoided! \\o/\n");
-
 return (0);
 }
