6aee3936fd12cd934f7318729ce0eb10c52888d2
Iamtunny <maestrotunexxxie@gmail.com>
2022-03-25 08:42:31 -0700

    new commit

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -7,9 +7,11 @@
  */
 
 void reverse_array(int a*, int n)
+
 {
 int *start_c, *end_c, c;
 int i;
+
 
 start_c = a;
 end_c = a;
@@ -20,8 +22,12 @@ for (i = 0; i < n - 1; i++)
 end_c++;
 }
 
+
+
+
 for (i = 0; i < n / 2; i++)
 {
+
 
 c = *end_c;
 *end_c = *start_c;
