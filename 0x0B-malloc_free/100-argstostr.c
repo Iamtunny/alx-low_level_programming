c20f702dd6f964c3fef8d5fc2d57ae45119fd27d
Iamtunny <maestrotunexxxie@gmail.com>
2022-04-05 10:05:44 -0700

    my commit

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -3,12 +3,13 @@
  * argstostr - prints args
  * @ac: takes in width of grid
  * @av: height of grid
- * Return: the args one line at a time 
+ * Return: the args one line at a time
  */
 
 char *argstostr(int ac, char **av)
 {
 char *str;
+
 int count = 0, a = 0, b = 0, c = 0;
 
 
