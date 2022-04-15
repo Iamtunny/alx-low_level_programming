92ded79e450ebcb03c37487e39c897051e772335
Iamtunny <maestrotunexxxie@gmail.com>
2022-04-15 14:58:31 -0700

    my commit

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -22,7 +22,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			printf("%s", str);
 		else
 			printf("(nil)");
-		
+
 		if (i < n - 1)
 			if (separator)
 				printf("%s", separator);
