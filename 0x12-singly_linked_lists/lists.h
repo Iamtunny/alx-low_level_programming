bf6971739c8f1e4b8390673f67bac05d039dc4e2
Iamtunny <maestrotunexxxie@gmail.com>
2022-04-22 00:39:56 -0700

    my commit

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -7,9 +7,9 @@
 
 /**
  * struct list_s - singly linked list
- * @str: string - (malloc string)
+ * @str: string - (malloc'ed string)
  * @len: length of the string
- * @next: pointsto the next node
+ * @next: points to the next node
  *
  * Description: singly linked list node structure
  * for holberton project
