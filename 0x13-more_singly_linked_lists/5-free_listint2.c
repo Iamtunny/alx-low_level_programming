6f247ae6c2617bd0e72682a42aa259c1fbd7642a
Iamtunny <maestrotunexxxie@gmail.com>
2022-04-26 07:56:48 -0700

    my commit

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -2,12 +2,13 @@
 /**
  * free_listint2 - function frees a listint_t list
  * @head: is a pointer to a linked list
- * Return: is alwayss void
+ * Return: is always void
  */
 
 void free_listint2(listint_t **head)
 {
 	listint_t *tmp;
+
 	if (head == NULL)
 		return;
 
