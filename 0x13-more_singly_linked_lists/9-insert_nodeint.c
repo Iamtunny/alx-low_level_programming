86ba7990e8f67f2457113a4446bcdd3207b14b49
Iamtunny <maestrotunexxxie@gmail.com>
2022-04-26 23:27:17 -0700

    my commit

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -10,7 +10,7 @@
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	unsigned inti;
+	unsigned int i;
 	listint_t *new;
 	listint_t *temp = *head;
 
@@ -18,7 +18,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (!new || !head)
 		return (NULL);
 
-	new-> = n;
+	new->n = n;
 	new->next = NULL;
 
 	if (idx == 0)
