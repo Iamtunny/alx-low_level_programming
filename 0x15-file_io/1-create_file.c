46fa53ef1ec9552fef3cad50f178667487c0e45f
Iamtunny <maestrotunexxxie@gmail.com>
2022-05-05 18:40:01 -0700

    my commit

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	/* open a file and truncate if exist, else create and set to write only */
-	fd = open(filename, O_CREAT | 0_WRONLY | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (fd == -1)
 		return (-1);
